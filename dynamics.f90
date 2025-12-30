0bd4d866ac40d7fe86b896f2c036a5e7472a3047
Monica <monica.mancinelli@studenti.unipg.it>
2025-12-30 16:27:15 +0100

    one function, one subroutine

--- dynamics.f90
+++ dynamics.f90
@@ -1,31 +1,49 @@
       program dynamics
 !Allocating and freeing 2D arrays
- 
-      double precision, allocatable :: a(:,:)
+      implicit none
+      integer :: input_file, Natoms, atoms
+      double precision, allocatable :: coord(:,:)
+      double precision, allocatable :: mass(:)
       integer :: i_stat
+      input_file = 9
+      open(input_file, file='inp.txt')
+      open(10,file='dynamics.xyz')
+      atoms = read_Natoms(input_file,Natoms)
+      write(6,*) atoms
+      call read_molecule(input_file, Natoms, coord, mass)
+!      DO i=1,Natoms
+!        write(6, *) (coord(i,j), j=1,3), mass(i)
+!      END DO
+contains
+      integer function read_Natoms(input_file,Natoms) 
+      implicit none
+      integer, intent(in) :: input_file
+      integer :: Natoms
+      read(input_file, *) Natoms
+      end function read_Natoms
 
-      allocate(a(m,n), stat=i_stat)
+      subroutine read_molecule(input_file, Natoms, coord, mass)
+      implicit none
+      integer, intent(in) :: input_file
+      integer, intent(in) :: Natoms
+      double precision, allocatable, intent(out) :: coord(:,:)
+      double precision, allocatable, intent(out) :: mass(:)
+      integer :: i_stat
+      allocate(coord(Natoms,3), stat=i_stat)
       if (i_stat /= 0) then
-                print *,  "Memory allocation failed!"
+                print *,  "Memory allocation failed, for coord!"
                 stop
         end if
-
-        deallocate(a)
-
-      subroutine read_molecule(inp.txt, Natoms, coord, mass)
-      integer, intent(in) :: inp.txt
-      integer, intent(in) :: Natoms
-      double precision, intent(out) :: coord(Natoms,3)
-      double precision, intent(out) :: mass(Natoms)
-      !reads the atomic coordinates and masses and stores the data in
-      !the arrays coord and mass provided as parameters. coord is a
-      !two-dimensinal array distance of size (Natoms*Natoms)
+      allocate(mass(Natoms), stat=i_stat)
+      if (i_stat /= 0) then
+                print *,  "Memory allocation failed, for mass!"
+                stop
+        end if
+      DO i=1,Natoms
+        read(input_file, *) (coord(i,j), j=1,3), mass(i)
+      END DO 
+      DO i=1,Natoms
+        write(6, *) (coord(i,j), j=1,3), mass(i)
+      END DO
       end subroutine read_molecule
-
-      subroutine compute_distances(Natoms, coord, distance)
-      implicit none 
-      integer, intent(in) :: Natoms
-      double precision, intent(in) :: coord(Natoms,3)
-      double precision, intent(out) :: coord(Natoms,Natoms)
-      end subroutine compute_distances
-
+end program dynamics
